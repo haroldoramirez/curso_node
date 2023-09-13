c8d568aa6438d18e22135eeaea471555e579dadf
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2023-09-13 09:10:16 -0300

    feat: adicionado eslint e prettier

--- app.js
+++ app.js
@@ -4,35 +4,35 @@ const PORT = 8080;
 
 const app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hellow World TESTE');
+app.get('/', function (req, res) {
+  res.send('Hellow World TESTE');
 });
 
-app.get('/user', function(req, res) {
-    res.send({
-        nome: 'José'
-    });
+app.get('/user', function (req, res) {
+  res.send({
+    nome: 'Haroldo',
+  });
 });
 
-app.get('/user/:nome', function(req, res) {
-    res.send({
-        nome: req.params.nome
-    });
+app.get('/user/:nome', function (req, res) {
+  res.send({
+    nome: req.params.nome,
+  });
 });
 
-app.get('/user/:nome/sobrenome/:sobrenome', function(req, res) {
-    res.send({
-        nome: req.params.nome,
-        sobrenome: req.params.sobrenome
-    });
+app.get('/user/:nome/sobrenome/:sobrenome', function (req, res) {
+  res.send({
+    nome: req.params.nome,
+    sobrenome: req.params.sobrenome,
+  });
 });
 
-app.delete('/user', function(req, res) {
-    res.send({
-        nome: 'José deletado'
-    });
+app.delete('/user', function (req, res) {
+  res.send({
+    nome: 'Haroldo deletado',
+  });
 });
 
-app.listen(PORT, function() {
-    console.log('Servidor rodando na porta: ', PORT);
+app.listen(PORT, function () {
+  console.log('Servidor rodando na porta: ', PORT);
 });

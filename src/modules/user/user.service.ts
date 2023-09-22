d9423491faf534b14bae655592739f0c56c87f96
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2023-09-22 11:32:43 -0300

    adicionado tipagem no service user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from '@prisma/client';
+import { UserModel } from './user.module';
+import { UserInsertDTO } from './dtos/user-insert.dto';
 
 const prisma = new PrismaClient();
 
-export const getUsers = async () => {
+export const getUsers = async (): Promise<UserModel[]> => {
   return prisma.user.findMany();
 };
 
-export const createUser = async (body) => {
+export const createUser = async (body: UserInsertDTO): Promise<UserModel => {
   console.log('body', body);
   return prisma.user.create({
     data: body,

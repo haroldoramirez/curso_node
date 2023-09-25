d6b185a288b077818be98abcc9a59d282f94112c
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2023-09-25 12:34:10 -0300

    tratamento de erros no service e controller do usuario

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { UserModel } from './user.module';
 import { UserInsertDTO } from './dtos/user-insert.dto';
 import { NotFoundException } from '@exceptions/not-found-exception';
+import { BadRequestException } from '@exceptions/bad-request-exception';
 
 const prisma = new PrismaClient();
 
@@ -15,8 +16,48 @@ export const getUsers = async (): Promise<UserModel[]> => {
   return prisma.user.findMany();
 };
 
+export const getUserByEmail = async (email: string): Promise<UserModel> => {
+  const user = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    throw new NotFoundException('Usuário');
+  }
+
+  return user;
+};
+
+export const getUserByCpf = async (cpf: string): Promise<UserModel> => {
+  const user = await prisma.user.findFirst({
+    where: {
+      cpf,
+    },
+  });
+
+  if (!user) {
+    throw new NotFoundException('User');
+  }
+
+  return user;
+};
+
 export const createUser = async (body: UserInsertDTO): Promise<UserModel> => {
-  console.log('body', body);
+  
+  const emailUsuario = await getUserByEmail(body.email).catch(() => undefined);
+
+  if (emailUsuario) {
+    throw new BadRequestException('Email encontrado no banco de dados');
+  }
+
+  const emailCPF = await getUserByCpf(body.cpf).catch(() => undefined);
+
+  if (emailCPF ) {
+    throw new BadRequestException('CPF encontrado no banco de dados');
+  }
+
   return prisma.user.create({
     data: body,
   });

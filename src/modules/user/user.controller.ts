401f7154d4bb1d157e1fb202a630fa4be5db347d
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2023-10-05 18:22:07 -0300

    feat: adicionado middleware na ordem para atender as regras do user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,22 +3,28 @@ import { createUser, getUsers } from './user.service';
 import { UserInsertDTO } from './dtos/user-insert.dto';
 import { NotFoundException } from '@exceptions/not-found-exception';
 import { ReturnError } from '@exceptions/dtos/return-error-dto';
-import { verifyToken } from '@utils/auth';
+import { authMiddleware } from 'src/middlewares/auth.middleware';
 
 const userRouter = Router();
 
 const router = Router();
 
+// Ordem 1
+//Necessario para nao interceptar com o middleware
+router.post('/', async (req: Request<undefined, undefined, UserInsertDTO>, res: Response): Promise<void> => {
+    const usuario = await createUser(req.body).catch((error) => {
+      new ReturnError(res, error);
+    });
+    res.send(usuario);
+  },
+);
+
+router.use(authMiddleware);
+
 userRouter.use('/user', router);
 
 //Inicio da rota depois do user - tipo o index
 router.get('/', async (req: Request, res: Response): Promise<void> => {
-  const authorization = req.headers.authorization;
-
-  verifyToken(authorization).catch((error) => {
-    new ReturnError(res, error);
-  });
-
   const listaUsuarios = await getUsers().catch((error) => {
     if (error instanceof NotFoundException) {
       res.status(204);
@@ -28,22 +34,5 @@ router.get('/', async (req: Request, res: Response): Promise<void> => {
   });
   res.send(listaUsuarios);
 });
-
-router.get('/:nome/sobrenome/:sobrenome', async (req: Request, res: Response): Promise<void> => {
-  res.send({
-    nome: req.params.nome,
-    sobrenome: req.params.sobrenome,
-  });
-});
-
-router.post(
-  '/',
-  async (req: Request<undefined, undefined, UserInsertDTO>, res: Response): Promise<void> => {
-    const usuario = await createUser(req.body).catch((error) => {
-      new ReturnError(res, error);
-    });
-    res.send(usuario);
-  },
-);
 
 export default userRouter;

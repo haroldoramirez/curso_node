57d5f7ba2513d2023294d4aafb6c54036c696683
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2023-10-05 11:33:16 -0300

    feat: adicionado jwt

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { AuthDTO } from './dtos/auth.dto';
+import { getUserByEmail } from '@modules/user/user.service';
+import { validatePassword } from '@utils/password';
+import { NotFoundException } from '@exceptions/not-found-exception';
+import { AuthModel } from './auth.model';
+import { generateToken } from '@utils/auth';
+
+export const validateAuth = async (authDTO: AuthDTO): Promise<AuthModel> => {
+  const user = await getUserByEmail(authDTO.email);
+
+  const isValidPassword = await validatePassword(authDTO.password, user.password);
+
+  if (!isValidPassword) {
+    throw new NotFoundException('User');
+  }
+
+  return new AuthModel(generateToken(user), user);
+};
---
+++ src/utils/auth.ts
@@ -0,0 +1,37 @@
+import { UnauthorizedException } from '@exceptions/unauthorized-exception';
+import { UserAuthDTO } from '@modules/auth/dtos/user.auth.dto';
+import { UserModel } from '@modules/user/user.module';
+import { sign, verify } from 'jsonwebtoken';
+
+export const PASSWORD_JWT = 'senhadepoismudar';
+
+export const generateToken = (user: UserModel): string => {
+  return sign(
+    {
+      userId: user.id, // 1 - Payload
+      email: user.email,
+      tipo: user.typeUser,
+    } as UserAuthDTO,
+    PASSWORD_JWT, // 2 - Password Token variavel de ambiente
+    {
+      subject: String(user.id), // 3 Subject
+      expiresIn: '604800000',
+    },
+  );
+};
+
+export const verifyToken = async (authorization?: string): Promise<UserAuthDTO> => {
+  if (!authorization) {
+    throw new UnauthorizedException();
+  }
+
+  const [, token] = authorization.split(' ');
+
+  try {
+    const decodedToken = <UserAuthDTO>verify(token, PASSWORD_JWT);
+
+    return decodedToken;
+  } catch (error) {
+    throw new UnauthorizedException();
+  }
+};
